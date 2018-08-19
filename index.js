ba5d78d66706505ad89ec564a9ab0d08d7656dda
Leanne Cabey <leanne.m.cabey@gmail.com>
2018-08-19 23:47:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ function placeOrder(cardNumber) {
   if (typeof cardNumber == 'undefined') {
     return "Sorry, we don't have a credit card on file for you."
   } else {
+    totalCost = total()
     cart = []
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`
   }
 }
