9eb0a7c2bf544f17a4b656054f194bb5a35f47db
Leanne Cabey <leanne.m.cabey@gmail.com>
2018-08-19 22:03:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function addToCart(item) {
 
 function viewCart() {
   if (cart.length == 0) {
-    return 'Your shopping cart is empty'
+    return 'Your shopping cart is empty.'
   }
   else {
     var itemList = []
