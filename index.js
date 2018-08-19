a1882abb88c121650a649e7f77b52c0b8abacb84
Leanne Cabey <leanne.m.cabey@gmail.com>
2018-08-19 22:28:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
     for (var i = 0; i < cart.length; i++) {
     itemList.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
     }
-    return `In your cart, you have ${itemList.toString()}.`
+    return `In your cart, you have${itemList.toString()}.`
   }
 }
 
